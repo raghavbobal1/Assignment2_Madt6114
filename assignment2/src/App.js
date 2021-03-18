b31e7b924d858a76d605a1d531a2a865d2f3f504
raghavbobal1 <raghavbobal001@gmail.com>
2021-03-18 19:57:28 +0530

    final commit

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useRef, useState} from 'react'
 
 function App() {
+
+  const userName = useRef("");
+  const password = useRef("");
+  const isRemembered = useRef(false);
+
+  // over kill but code for just using bot useref and use state. useRef doesnt kick rerender so using useState
+  const [userNameState, setUserNameState] = useState("")
+  const [passwordState, setPasswordState] = useState("")
+  const [isRememberedState, setIsRememberedState] = useState(false)
+  const [isFormDataShown, setisFormDataShown] = useState(false);
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    setUserNameState(userName.current.value);
+    setPasswordState(password.current.value);
+    setIsRememberedState(isRemembered.current.checked)
+    setisFormDataShown(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <form className="form">
+          <div className="form-control">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" ref={userName}></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" ref={password}></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="remember">Remember</label>
+            <input type="checkbox" id="remember" ref={isRemembered}></input>
+          </div>
+          <button type="button" onClick={clickHandler}>
+            Login
+          </button>
+        </form>
+      </div>
+
+      {isFormDataShown && (
+        <div className="container">
+          <h4> You submitted following data:</h4>
+          <p> Username: {userNameState}</p>
+          <p> Password: {passwordState}</p>
+          <p> Your data is {!isRememberedState && "not"} remembered.</p>
+        </div>
+      )}
+    </>
   );
 }
 
